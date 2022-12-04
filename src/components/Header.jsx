a68088884e1c15c17c583dba7102badaebcf2d78
joselyncondoricabrera <condori.cabrera.joselyn@gmail.com>
2022-12-04 09:38:40 -0500

    probando cambio con netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
             <div className="navbar-right">
                 <ul>
                     <li className="navbar-email" onClick={handleToggle}>
-                        platzi@example.com
+                        danitza.condori.cabrera@gmail.com
                     </li>
                     <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
                         <img src={shoppingCart} alt="shopping cart" />
